c9809826515f1c361b9a1c259931e2b441e2ef51
Mohamed Hamza <manwar@aquilaos.com>
2021-01-22 06:30:06 +0200

    add Arc<T> structure

--- src/ds/mod.rs
+++ src/ds/mod.rs
@@ -4,6 +4,7 @@ mod hashmap;
 mod ringbuffer;
 mod bitmap;
 mod buddy;
+mod arc;
 
 pub use self::buffer::*;
 pub use self::queue::*;
@@ -11,3 +12,4 @@ pub use self::hashmap::*;
 pub use self::ringbuffer::*;
 pub use self::bitmap::*;
 pub use self::buddy::*;
+pub use self::arc::*;
